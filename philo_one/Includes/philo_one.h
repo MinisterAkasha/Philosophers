3bbabf8c4961e20213ba27e8ef647a1c3a3a7c72
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-04 11:44:31 +0300

    moved structures to a separate header

--- philo_one/Includes/philo_one.h
+++ philo_one/Includes/philo_one.h
@@ -7,45 +7,11 @@
 # include <pthread.h>
 # include <sys/time.h>
 
+# include "structures.h"
 # include "utils.h"
 # include "validate.h"
+# include "write_message.h"
 
-typedef enum e_Bool {
-	FALSE,
-	TRUE
-}	t_Bool;
-
-typedef enum e_state_type {
-	EATING,
-	THINKING,
-	SLEEPING,
-	IS_DEAD
-}	t_state_type;
-
-typedef struct s_philos_options {
-	int	p_num;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	times_need_to_eat;
-}				t_philos_options;
-
-typedef struct s_philo_state {
-	int			state;
-	int			position;
-	pthread_t	philo_thread;
-}				t_philo_state;
-
-typedef struct s_state {
-	int					start_time;
-	t_philo_state		*philo;
-	pthread_mutex_t		*forks;
-	pthread_mutex_t		message_mutex;
-	pthread_t			observer_pt;
-
-	int					error;
-	t_philos_options	philo_options;
-}				t_state;
 
 int init(t_state *state, char **argv);
 
