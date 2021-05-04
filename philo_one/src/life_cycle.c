caa06c7dcde079cbc3d1ab3b40ab37038ea19109
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-04 12:49:26 +0300

    add needed forks definition

--- philo_one/src/life_cycle.c
+++ philo_one/src/life_cycle.c
@@ -9,16 +9,30 @@ int	get_philo_pos(t_state *state)
 	return (pos);
 }
 
+void get_forks_index(int pos, int philo_num, int (*forks)[2])
+{
+	(*forks)[0] = pos;
+	if (pos == philo_num - 1)
+		(*forks)[1] = 0;
+	else
+		(*forks)[1] = pos + 1;
+}
+
 void* philo_life_cycle(void *args)
 {
 	t_state *state;
 	int		pos;
+	int		forks[2];
 
 	state = (t_state*) args;
 	pos = get_philo_pos(state);
-	// pthread_mutex_lock(&state->forks[0]);
+	get_forks_index(pos, state->philo_options.p_num, &forks);
+
+	pthread_mutex_lock(&state->forks[forks[0]]);
+	pthread_mutex_lock(&state->forks[forks[1]]);
 
 	// printf("1 :>> %d\n", 1);
-	// pthread_mutex_unlock(&state->forks[0]);
+	pthread_mutex_unlock(&state->forks[forks[0]]);
+	pthread_mutex_unlock(&state->forks[forks[1]]);
 	return (NULL);
 }
