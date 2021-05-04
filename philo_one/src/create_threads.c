56f9361f0106b3b0c1877341c078ee183d507ea5
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-04 11:37:04 +0300

    added a check for the status of a philosopher in the observer

--- philo_one/src/create_threads.c
+++ philo_one/src/create_threads.c
@@ -1,15 +1,23 @@
 #include "philo_one.h"
 
-int a;
-
 void* observer(void *args)
 {
 	t_state *state;
+	int		i;
 
 	state = (t_state*) args;
 	while (1)
 	{
-		
+		i = 0;
+		while (i < state->philo_options.p_num)
+		{
+			if (state->philo[i].state == IS_DEAD)
+			{
+				//TODO write_mesasge();
+				return (NULL);
+			}
+			i++;
+		}
 	}
 	return (NULL);
 }
@@ -20,29 +28,24 @@ void* philo_life_cycle(void *args)
 
 	state = (t_state*) args;
 	pthread_mutex_lock(&state->forks[0]);
-	for (int i = 0; i < 20; i++)
-		a++;
+	printf("1 :>> %d\n", 1);
 	pthread_mutex_unlock(&state->forks[0]);
 	return (NULL);
 }
 
 int create_threads(t_state *state)
 {
-	int status;
 	int i;
 
 	i = 0;
 	while (i < state->philo_options.p_num)
 	{
-		//TODO обработать ошибки
-		status = pthread_create(&state->philo[i].philo_thread, NULL, philo_life_cycle, state);
-		if (status != 0)
+		if (pthread_create(&state->philo[i].philo_thread, NULL, philo_life_cycle, state) != 0)
 			return (1);
 		pthread_detach(state->philo[i].philo_thread);
 		i++;
 	}
-	status = pthread_create(&state->observer_pt, NULL, observer, state);
-	if (status != 0)
+	if (pthread_create(&state->observer_pt, NULL, observer, state) != 0)
 		return (1);
 	pthread_join(state->observer_pt, NULL);
 	return (0);
