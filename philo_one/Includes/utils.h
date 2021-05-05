1d80abdf7efdb5973e5da7f9cf7046aa3a49f5ab
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-05 15:55:46 +0300

    remove ft_put__fd from utils

--- philo_one/Includes/utils.h
+++ philo_one/Includes/utils.h
@@ -9,10 +9,7 @@
 
 long int	ft_atoi(const char *str);
 size_t		ft_strlen(const char *s);
-void		ft_putstr_fd(char *s, int fd);
 int			ft_isdigit(int c);
-void		ft_putnbr_fd(int n, int fd);
-void		ft_putchar_fd(char c, int fd);
 long int	get_current_time(t_state *state);
 
 #endif
