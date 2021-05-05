a64a3568ca851405ba6b61b83ad77121bf699fd1
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-05 11:21:34 +0300

    add take and put forks functions

--- philo_one/src/life_cycle.c
+++ philo_one/src/life_cycle.c
@@ -19,10 +19,37 @@ void get_forks_index(int pos, int philo_num, int (*forks)[2])
 		(*forks)[1] = pos + 1;
 }
 
+void put_forks(pthread_mutex_t left_fork, pthread_mutex_t right_fork, int pos)
+{
+	if ((pos & 1) == TRUE)//1-3-5...
+	{
+		pthread_mutex_unlock(&left_fork);
+		pthread_mutex_unlock(&right_fork);
+	}
+	else if ((pos & 1) == FALSE)//0-2-4...
+	{
+		pthread_mutex_unlock(&right_fork);
+		pthread_mutex_unlock(&left_fork);
+	}
+}
+
+void take_forks(pthread_mutex_t left_fork, pthread_mutex_t right_fork, int pos)
+{
+	if ((pos & 1) == TRUE)//1-3-5...
+	{
+		pthread_mutex_lock(&right_fork);
+		pthread_mutex_lock(&left_fork);
+	}
+	else if ((pos & 1) == FALSE)//0-2-4...
+	{
+		pthread_mutex_lock(&left_fork);
+		pthread_mutex_lock(&right_fork);
+	}
+}
+
 void philo_eating(t_state *state, int pos, int forks[2])
 {
-	pthread_mutex_lock(&state->forks[forks[0]]);
-	pthread_mutex_lock(&state->forks[forks[1]]);
+	take_forks(state->forks[forks[0]], state->forks[forks[1]], pos);
 	state->philo[pos].state = IS_TAKE_FORK;
 	write_message(state, pos);
 	write_message(state, pos);
@@ -31,8 +58,7 @@ void philo_eating(t_state *state, int pos, int forks[2])
 	write_message(state, pos);
 	state->philo[pos].eat_times++;
 	usleep(state->philo_options.time_to_eat * 1000);
-	pthread_mutex_unlock(&state->forks[forks[0]]);
-	pthread_mutex_unlock(&state->forks[forks[1]]);
+	put_forks(state->forks[forks[0]], state->forks[forks[1]], pos);
 }
 
 void philo_sleeping(t_state *state, int pos)
