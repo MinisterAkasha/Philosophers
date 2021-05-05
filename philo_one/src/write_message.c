f04342f70271cbf4b9c2d4f139bdc77f3ea4e537
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-05 15:58:46 +0300

    return ft_put__fd to utils

--- philo_one/src/write_message.c
+++ philo_one/src/write_message.c
@@ -5,6 +5,10 @@
 void write_message(t_state *state, int index)
 {
 	pthread_mutex_lock(&state->message_mutex);
-	printf("%ld %d %s", get_current_time(state), index + 1, state->str_message[state->philo[index].state]);
+	ft_putnbr_fd(get_current_time(state), 1);
+	ft_putchar_fd(' ', 1);
+	ft_putnbr_fd(index + 1, 1);
+	ft_putchar_fd(' ', 1);
+	ft_putstr_fd(state->str_message[state->philo[index].state], 1);
 	pthread_mutex_unlock(&state->message_mutex);
 }
