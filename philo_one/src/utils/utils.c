a90273c5e28d48b3df1812256e26827ddb39c377
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-05 16:17:59 +0300

    split utils functions to several files

--- philo_one/src/utils/utils.c
+++ philo_one/src/utils/utils.c
@@ -44,48 +44,6 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-void	ft_putstr_fd(char *s, int fd)
-{
-	if (!s || !fd)
-		return ;
-	write(fd, s, ft_strlen(s));
-}
-
-int	ft_isdigit(int c)
-{
-	if (c >= 48 && c <= 57)
-		return (1);
-	return (0);
-}
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	if (n <= -2147483648)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		ft_putnbr_fd(147483648, fd);
-	}
-	else if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(n * (-1), fd);
-	}
-	else if (n >= 10)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putchar_fd(n % 10 + '0', fd);
-	}
-	else
-		ft_putchar_fd(n + '0', fd);
-}
-
-void	ft_putchar_fd(char c, int fd)
-{
-	if (fd >= 0)
-		write(fd, &c, 1);
-}
-
 long int	get_current_time(t_state *state)
 {
 	static struct timeval	tv;
