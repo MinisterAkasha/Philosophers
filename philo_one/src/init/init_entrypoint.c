7fc97cc8d278b57ec389b71b16b81bbb456688de
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-06 12:42:35 +0300

    fix error

--- philo_one/src/init/init_entrypoint.c
+++ philo_one/src/init/init_entrypoint.c
@@ -10,7 +10,7 @@ int	init(t_state *state, char **argv)
 	init_start_time(&state->start_time);
 	init_mutex(state, state->philo_options.p_num, &error);
 	init_philosophers(&state->philo, state->philo_options.p_num, \
-		state->start_time, error);
+		state->start_time, &error);
 	init_str_message(state->str_message);
 	state->philo_pos = 0;
 	return (error);
--- philo_two/src/free.c
+++ philo_two/src/free.c
@@ -7,9 +7,5 @@ void	free_everything(t_state *state)
 	sem_unlink(SEM_MESSAGE);
 	sem_unlink(SEM_PHILO_TAKE_FORKS);
 	sem_unlink(SEM_PHILO_POS);
-	// sem_close(state->forks_sem);
-	// sem_close(state->message_sem);
-	// sem_close(state->take_forks_sem);
-	// sem_close(state->philo_pos_sem);
 	free(state);
 }
