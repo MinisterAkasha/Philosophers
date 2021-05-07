3235229d4c74e2ccfb3aeba6608ffaefc9b9a914
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-05-07 15:42:18 +0300

    fix message after philos dead

--- philo_three/src/observer.c
+++ philo_three/src/observer.c
@@ -10,10 +10,12 @@ void	*observer(void *args)
 		if (get_current_time(state) - state->philo[state->philo_pos].last_eat \
 			> state->philo_options.time_to_die)
 		{
+			sem_wait(state->message_sem);
 			sem_wait(state->dead_philo_sem);
 			state->philo[state->philo_pos].state = IS_DEAD;
-			write_message(state, state->philo_pos);
-			sem_wait(state->message_sem);
+			printf("%ld %d %s", get_current_time(state), \
+			state->philo_pos + 1, \
+			state->str_message[state->philo[state->philo_pos].state]);
 			exit(1);
 		}
 	}
